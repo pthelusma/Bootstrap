68952be25f78837371a605fff264a840c56aa91b
Pierre Thelusma <pthelusma@gmail.com>
2015-02-02 09:43:27 -0500

    Adding more to Home Page. Added controller action for submit

--- MvcApplication5/Controllers/HomeController.cs
+++ MvcApplication5/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +28,41 @@ namespace MvcApplication5.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Submit(string remoteip, string challenge, string response)
+        {
+
+            HttpClient client = new HttpClient();
+
+            string privatekey = "6LcXkfUSAAAAAGQO8dXYce09FzVPyMRoCK2ONzHo";
+
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("remoteip", HttpUtility.UrlEncode(remoteip)));
+            values.Add(new KeyValuePair<string, string>("privatekey", HttpUtility.UrlEncode(privatekey)));
+            values.Add(new KeyValuePair<string, string>("challenge", HttpUtility.UrlEncode(challenge)));
+            values.Add(new KeyValuePair<string, string>("response", HttpUtility.UrlEncode(response)));
+
+            var content = new FormUrlEncodedContent(values);
+            var postResponse = client.PostAsync("http://www.google.com/recaptcha/api/verify", content).Result;
+
+            string resultContent = postResponse.Content.ReadAsStringAsync().Result;
+
+            string[] responseResults = resultContent.Split(new string[] { "\n", "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Response resp = new Response()
+            {
+                result = responseResults[0],
+                code = responseResults[1]
+            };
+
+            return Json(resp);
+        }
+
+        public class Response
+        {
+            public string result { get; set; }
+            public string code { get; set; }
         }
     }
 }
---
+++ MvcApplication5/Scripts/Views/ReCaptcha.js
@@ -0,0 +1,73 @@
+﻿var ReCaptcha = (function () {
+
+    var challenge;
+    var response;
+
+    var init = function () {
+        createReCaptcha();
+    }
+
+    var createReCaptcha = function () {
+        Recaptcha.create("6LcXkfUSAAAAACBhoS9s68a6Ivu_kCfoo8RU1-hg", "ReCaptcha", {
+            theme: "red",
+            callback: Recaptcha.focus_response_field
+        });
+    }
+
+    var destroyReCaptcha = function () {
+        Recaptcha.destroy();
+    }
+
+    var recreateRecaptcha = function () {
+        destroyReCaptcha();
+        createReCaptcha();
+    }
+
+    function isValidResponse() {
+        return (reponse && response != "");
+    }
+
+    function storeRecaptchaParameters() {
+        challenge = Recaptcha.get_challenge();
+        reponse = Recaptcha.get_response();
+    }
+
+    function isValidReCaptcha() {
+
+        var remoteip;
+
+        var success = false;
+
+        $.getJSON("http://jsonip.com",
+            function (res) {
+                remoteip = res.ip;
+            });
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/Submit",
+            async: false,
+            data: {
+                remoteip: remoteip,
+                challenge: Recaptcha.get_challenge(),
+                response: Recaptcha.get_response()
+            },
+            success: function (resp) {
+                if (resp.result === "true") {
+                    success = true;
+                } else {
+                    recreateRecaptcha();
+                }
+            }
+        });
+
+        return success;
+    }
+
+    return {
+        init: init,
+        isValidReCaptcha: isValidReCaptcha,
+        isValidResponse: isValidResponse,
+        storeRecaptchaParameters: storeRecaptchaParameters
+    };
+})();
